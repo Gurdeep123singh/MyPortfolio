751d8bdaa4bc7ad8822b9c0566bf3cec0d1d7210
Gurdeep123singh <yanio2617275@gmail.com>
2022-07-10 19:13:42 +0530

    some projects have been uploaded, changes in styling and js have been done

--- js/index.js
+++ js/index.js
@@ -2,22 +2,24 @@ $(document).ready(function(){
   $(window).scroll(function(){
     if(this.scrollY>20){
       $('.navbar').addClass('sticky');
+      $('.navbar-brand').addClass('sticky');
+      
     }
     else{
       $('.navbar').removeClass('sticky');
+      $('.navbar-brand').removeClass('sticky');
+     
     }
   });
 });
 
-// $(function () {
-//   $(document).scroll(function () {
-//     var $nav = $(".navbar-fixed-top");
-//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-//   });
-// });
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
 
-// project Section
 
-function makeUrl(){
-  window.open("file:///C:/Users/Ronak/Desktop/web%20development/Drum%20Kit%20Starting%20Files/index.html","_blank");
-}
+
+
